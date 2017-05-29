7e8600bfc964cb88146b645dadd5b2f49f2e2268
Byrne Hollander <bhollander823@gmail.com>
2017-05-28 21:35:05 -0400

    fixed import statement in report controller

--- app/controllers/report_controller.js
+++ app/controllers/report_controller.js
@@ -1,13 +1,18 @@
-import { ReportModel } from '../models/post_model';
+import { ReportModel } from '../models/report_model';
 
 const createReport = (req, res) => {
   const { reporter, item, type, severity, additionalInfo } = req.body;
   const r = new ReportModel({ reporter, severity, additionalInfo });
   if (type === 'COMMENT') {
     r.reportedComment = item;
+    // r.reportedPost = null;
   } else if (type === 'POST') {
     r.reportedPost = item;
+    // r.reportedComment = null;
   }
+  // else {
+  //   return console.log('error: must be of type COMMENT or POST');
+  // }
   r.timestamp = Date.now();
   r.save()
     .then((result) => {
